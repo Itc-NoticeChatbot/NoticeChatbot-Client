9686bc01e768a4b897d8e11d000e6c7289d10975
huniversal <hunivers0523@naver.com>
2026-06-03 13:02:33 +0900

    fix: SearchInput 빈 인터페이스 type으로 교체

--- src/shared/ui/search-input/SearchInput.tsx
+++ src/shared/ui/search-input/SearchInput.tsx
@@ -2,7 +2,7 @@ import type { InputHTMLAttributes } from 'react'
 
 import { cn } from '../lib/cn'
 
-interface SearchInputProps extends InputHTMLAttributes<HTMLInputElement> {}
+type SearchInputProps = InputHTMLAttributes<HTMLInputElement>
 
 export function SearchInput({ className, type = 'search', ...props }: SearchInputProps) {
   return (
